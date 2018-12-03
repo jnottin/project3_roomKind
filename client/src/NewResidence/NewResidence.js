7aa1adc701bd374740bbdfcffb1d50b0745f86bb
Sam Nottingham <jnottin92@gmail.com>
2018-12-03 12:56:59 -0500

    second commit, addded drop downs and columns

---
+++ client/src/NewResidence/NewResidence.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
+import './NewResidence.css';
+
+
+class NewResidence extends Component {
+
+    render() {
+        return (
+            <div id="newResidenceForm" className="closed newResidenceForm" >
+                <p>RESIDENCE SIDE BAR</p>
+
+            </div>
+        );
+    }
+}
+
+export default NewResidence;
