4b225a6e12fda6e02bf9bb157f5355116d0f0ff6
Bijay Rai <raibjraibj@yahoo.com>
2018-12-03 16:36:51 -0500

    deleted router

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from './NavBar/NavBar.js';
-import Shelters from './Shelters/Shelters.js';
-import Residences from './Residences/Residences.js';
-import NewResidence from './NewResidence/NewResidence';
-import NewShelter from './NewShelter/NewShelter';
-import ReserveResidence from './ReserveResidence/ReserveResidence';
-
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+import NavBar from "./NavBar/NavBar.js";
+import Shelters from "./Shelters/Shelters.js";
+import Residences from "./Residences/Residences.js";
+import NewResidence from "./NewResidence/NewResidence";
+import NewShelter from "./NewShelter/NewShelter";
+import ReserveResidence from "./ReserveResidence/ReserveResidence";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        < NavBar />
-        < NewShelter />
-        < NewResidence />
+        <NavBar />
+        <NewShelter />
+        <NewResidence />
         <header className="App-header">
-          <div className="column">< Shelters /></div>
-          <div className="column">< Residences /></div>
+          <div className="column">
+            <Shelters />
+          </div>
+          <div className="column">
+            <Residences />
+          </div>
         </header>
       </div>
     );
--- client/src/ReserveResidence/ReserveResidence.js
+++ client/src/ReserveResidence/ReserveResidence.js
@@ -2,38 +2,38 @@ import React, { Component } from "react";
 import "./ReserveResidence.css";
 
 class ReserveResidence extends Component {
-    render() {
-        return (
-            <form className="form">
-                <div className="column">
-                    <h1>Reserve Residental</h1>
+  render() {
+    return (
+      <form className="form">
+        <div className="col">
+          <h1>Reserve Residental</h1>
 
-                    <div>
-                        <label>
-                            Arrival Time:
+          <div>
+            <label>
+              Arrival Time:
               <input type="text" name="time" />
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            Number of Beds:
+            </label>
+          </div>
+          <div>
+            <label>
+              Number of Beds:
               <input type="text" name="beds" />
-                        </label>
-                    </div>
+            </label>
+          </div>
 
-                    <div>
-                        <label>
-                            Number of Days:
+          <div>
+            <label>
+              Number of Days:
               <input type="text" name="days" />
-                        </label>
-                    </div>
-                    <div>
-                        <button>Submit</button>
-                    </div>
-                </div>
-            </form>
-        );
-    }
+            </label>
+          </div>
+          <div>
+            <button className="btn">Submit</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default ReserveResidence;
