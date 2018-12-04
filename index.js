776dfb21c82819a6cb9f502d8d13ed2b567a9a8c
“Christina <cfthompson321@gmail.com>
2018-12-04 11:55:45 -0500

    getting new shelter post

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
-const express = require('express')
-const parser = require('body-parser')
-const cors = require('cors')
-const Residential = require('./models/residential.js')
-const Shelter = require('./models/shelter.js')
-const Guest = require('./models/guest.js')
+const express = require("express");
+const parser = require("body-parser");
+const cors = require("cors");
+const Residential = require("./models/residential.js");
+const Shelter = require("./models/shelter.js");
+const Guest = require("./models/guest.js");
 
-const app = express()
+const app = express();
 
-app.set('port', process.env.PORT || 3007)
-app.use(parser.json())
-app.use(cors())
+app.set("port", process.env.PORT || 3007);
+app.use(parser.json());
+app.use(cors());
 
-app.use(express.static('client/build'))
+app.use(express.static("client/build"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
-app.get('/api/roomKind', (req, res) => {
-    Shelter.find()
-        .then((books) => {
-            res.json(books)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
+app.get("/api/roomKind", (req, res) => {
+  Shelter.find()
+    .then(books => {
+      res.json(books);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 //New Shelter
-app.post('/', (req, res) => {
-    console.log(req.body)
-    Shelter.create({
-        name: req.body.name,
-        location: req.body.location,
-        // image: req.body.image,
-        beds: req.body.beds
-    }).then(shelter => {
-        res.send(shelter)
-    })
-})
+app.post("/", (req, res) => {
+  console.log(req.body);
+  Shelter.create({
+    name: req.body.name,
+    location: req.body.location,
+    // image: req.body.image,
+    beds: req.body.beds
+  }).then(shelter => {
+    res.send(shelter);
+  });
+});
 
-
-app.listen(3007, () => {
-})
+app.listen(3007, () => {});
